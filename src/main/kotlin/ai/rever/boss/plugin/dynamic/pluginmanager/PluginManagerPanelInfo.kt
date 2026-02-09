5ff82f9ee47c19c9a0825d6f104c35116578b1ef
Shivang <shivang.iitk@gmail.com>
2026-02-08 17:00:54 -0800

    fix: Change panel position to left.bottom

--- src/main/kotlin/ai/rever/boss/plugin/dynamic/pluginmanager/PluginManagerPanelInfo.kt
+++ src/main/kotlin/ai/rever/boss/plugin/dynamic/pluginmanager/PluginManagerPanelInfo.kt
@@ -1,7 +1,6 @@
 package ai.rever.boss.plugin.dynamic.pluginmanager
 
-import ai.rever.boss.plugin.api.Panel.Companion.right
-import ai.rever.boss.plugin.api.Panel.Companion.top
+import ai.rever.boss.plugin.api.Panel.Companion.left
 import ai.rever.boss.plugin.api.Panel.Companion.bottom
 import ai.rever.boss.plugin.api.PanelId
 import ai.rever.boss.plugin.api.PanelInfo
@@ -12,10 +11,11 @@ import androidx.compose.material.icons.outlined.Extension
  * Plugin Manager panel info for dynamic plugin.
  *
  * Displays installed plugins and plugin store in a sidebar panel.
+ * Position: left sidebar, bottom section.
  */
 object PluginManagerPanelInfo : PanelInfo {
-    override val id = PanelId("plugin-manager", 100)
+    override val id = PanelId("plugin-manager", 6)
     override val displayName = "Plugin Manager"
     override val icon = Icons.Outlined.Extension
-    override val defaultSlotPosition = right.top.bottom
+    override val defaultSlotPosition = left.bottom
 }
